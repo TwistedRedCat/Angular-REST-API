9dd829b3041d6e6060fbb42f52b4987f66e9cddf
Husin <mhusnifauzi@gmail.com>
2020-03-04 16:52:48 +0800

    Added post endpoint

--- controllers/garage.js
+++ controllers/garage.js
@@ -17,3 +17,32 @@ exports.getProducts = (req, res, next) => {
       }
     });
 };
+
+exports.postProducts = async (req, res, next) => {
+  console.log(req.body);
+  const imageUrl = req.body.imageUrl;
+  const title = req.body.title;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl
+  });
+  try {
+    const result = await product.save();
+    //   const user = await User.findById(req.userId);
+    //   user.posts.push(post);
+    //   await user.save();
+    res.status(201).json({
+      message: 'Post created successfully!',
+      product: product
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
